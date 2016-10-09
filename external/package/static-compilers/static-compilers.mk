e47103fa5d34de7121b9738871a8b0b7b9ccf775
Adam Baxter <voltagex@voltagex.org>
2016-10-09 23:31:26 +1100

    So close, yet so far - cannot find crtbegin.o when trying to build buildroot in buildroot

--- external/package/static-compilers/static-compilers.mk
+++ external/package/static-compilers/static-compilers.mk
@@ -9,6 +9,19 @@ endef
 define STATIC_COMPILERS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/bin/
 	cp $(@D)/usr/x86_64-unknown-linux/bin/* $(TARGET_DIR)/bin/
+	#ugly hack to avoid the include files and libraries being nuked by buildroot
+	(cd $(@D)/usr/; tar -cf $(TARGET_DIR)/usr/include.tar include)
+	(cd $(@D)/usr/; tar -cf $(TARGET_DIR)/usr/lib.tar lib)
+	cp -vp $(STATIC_COMPILERS_PKGDIR)/S30include_hack $(TARGET_DIR)/etc/init.d/
 endef
+
+#define STATIC_COMPILERS_TARGET_FINALIZE
+# see comment below
+#endef
+
+#so this isn't particularly documented well, and runs *just*
+#before the include files are deleted so it wohn't work
+#TARGET_FINALIZE_HOOKS += STATIC_COMPILERS_TARGET_FINALIZE
+#but would have been a really cool trick if it worked
 
 $(eval $(generic-package))
