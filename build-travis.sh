ed1336984a401293b16c4b2bc3f9a3fe135e94d4
Adam Baxter <voltagex@voltagex.org>
2017-12-07 15:48:21 +1100

    Make the shell script better

--- build-travis.sh
+++ build-travis.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e
 
-ls
-ls ../buildroot
-
+#silly dance because I'm caching something inside a path that will be a git repo
 if [ ! -f "../buildroot/Makefile" ]; then
+
+    if [ -d "../buildroot/dl" ]; then
+        mv ../buildroot/dl ../buildroot-dl || true
+    fi
+
     git clone --depth=1 https://github.com/buildroot/buildroot ../buildroot
+    mv ../buildroot-dl ../buildroot/dl
 fi
 
 make skeleton_defconfig && make source && ls ../buildroot
