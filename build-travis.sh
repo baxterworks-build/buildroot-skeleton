b2a4303027ec6223be0fba15143811f6ba46652f
Adam Baxter <voltagex@voltagex.org>
2017-12-07 15:30:07 +1100

    Is Travis running in the wrong path?

--- build-travis.sh
+++ build-travis.sh
@@ -4,4 +4,7 @@ if [ ! -d "../buildroot" ]; then
     git clone --depth=1 https://github.com/buildroot/buildroot ../buildroot
 fi
 
+pwd
+exit 0
+
 make skeleton_defconfig && make source && ls ../buildroot
