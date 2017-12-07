0d8e762da6e6f05e8321192fa2ae81d1033318f7
Adam Baxter <voltagex@voltagex.org>
2017-12-07 15:54:32 +1100

    Build for real

--- build-travis.sh
+++ build-travis.sh
@@ -12,4 +12,4 @@ if [ ! -f "../buildroot/Makefile" ]; then
     mv ../buildroot-dl ../buildroot/dl
 fi
 
-make skeleton_defconfig && make source && ls ../buildroot
+make skeleton_defconfig && make source && make
