8d9d1e3b6b53317993619da8d2eb6775125e9997
Adam Baxter <voltagex@voltagex.org>
2017-12-07 15:36:02 +1100

    Something strange is going on.

--- build-travis.sh
+++ build-travis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+ls
+ls ../buildroot
+
 if [ ! -d "../buildroot" ]; then
     git clone --depth=1 https://github.com/buildroot/buildroot ../buildroot
 fi
-
-pwd
-exit 0
 
 make skeleton_defconfig && make source && ls ../buildroot
