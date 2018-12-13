14e9004c2e16c4f135fa4cf12a0f979a6821ad7e
Adam Baxter <voltagex@voltagex.org>
2018-12-13 21:04:37 +1100

    buildroot prerequisites

--- build-ci.sh
+++ build-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 apt -y update
-apt -y install curl
+apt -y install curl make gcc g++ unzip bc bzip2 python
 
 mkdir ../buildroot && pushd ../buildroot
 curl -L https://github.com/buildroot/buildroot/archive/master.tar.gz | tar --strip-components 1 -zxf -
