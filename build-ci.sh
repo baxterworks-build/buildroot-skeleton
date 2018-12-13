0cf1ae009b195a96f209233fe54ea228462a9493
Adam Baxter <voltagex@voltagex.org>
2018-12-13 23:23:49 +1100

    libelf dependency

--- build-ci.sh
+++ build-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 apt -qq -y update
-apt -qq -y install curl git make gcc g++ unzip bc bzip2 libncurses5-dev python patch wget cpio rsync
+apt -qq -y install curl git make gcc g++ unzip bc bzip2 libncurses5-dev python patch wget cpio rsync libelf-dev
 
 mkdir ../buildroot && pushd ../buildroot
 curl -L https://github.com/buildroot/buildroot/archive/master.tar.gz | tar --strip-components 1 -zxf -
