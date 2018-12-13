f0c8432e9b349691a9565c0f9533f0f27d0728d3
Adam Baxter <voltagex@voltagex.org>
2018-12-13 20:36:51 +1100

    drone.io fixes

--- build-ci.sh
+++ build-ci.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-apt update && apt -y install git
+apt update && apt -y install --no-install-recommends git
 
 #silly dance because I'm caching something inside a path that will be a git repo
 if [ ! -f "../buildroot/Makefile" ]; then
 
-    if [ -d "../buildroot/dl" ]; then
-        mv ../buildroot/dl ../buildroot-dl || true
-    fi
+#    if [ -d "../buildroot/dl" ]; then
+#        mv ../buildroot/dl ../buildroot-dl || true
+#    fi
 
     git clone --depth=1 https://github.com/buildroot/buildroot ../buildroot
-    mv ../buildroot-dl ../buildroot/dl
+#    mv ../buildroot-dl ../buildroot/dl
 fi
 
 make skeleton_defconfig && make source && make -s
