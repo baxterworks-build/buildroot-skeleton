f9f2e0947897d1d1cd9159167391ee39ed126a9b
Adam Baxter <voltagex@voltagex.org>
2018-12-13 21:27:54 +1100

    Silent build?

--- build-ci.sh
+++ build-ci.sh
@@ -7,4 +7,4 @@ mkdir ../buildroot && pushd ../buildroot
 curl -L https://github.com/buildroot/buildroot/archive/master.tar.gz | tar --strip-components 1 -zxf -
 popd
 
-make skeleton_defconfig && make source && make -s
+make skeleton_defconfig && make -s
