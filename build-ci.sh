2e7cc28a747de0a47f03a62cd641c1ac2a23db37
Adam Baxter <voltagex@voltagex.org>
2018-12-16 00:17:10 +1100

    Silence build for now v2

--- build-ci.sh
+++ build-ci.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-apt -qq -y update
-apt -qq -y install curl git make gcc g++ unzip bc bzip2 libncurses5-dev python patch wget cpio rsync libelf-dev
+apt -qq update
+apt -qq install curl git make gcc g++ unzip bc bzip2 libncurses5-dev python patch wget cpio rsync libelf-dev
 
 mkdir ../buildroot && pushd ../buildroot
-curl -L https://github.com/buildroot/buildroot/archive/master.tar.gz | tar --strip-components 1 -zxf -
+curl -sL https://github.com/buildroot/buildroot/archive/master.tar.gz | tar --strip-components 1 -zxf -
 popd
 
 make skeleton_defconfig && make -s &> /dev/null
